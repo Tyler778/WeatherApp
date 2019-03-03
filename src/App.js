0e6b8faba138fda6022661b0d4df1dc5a94f5062
Tyler <tyler.quante@gmail.com>
2019-03-03 02:30:49 -0500

    finishing touches

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ export default class App extends Component {
     humidity: undefined,
     description: undefined,
     error: undefined,
-
   }
   getWeather = async (e) => {
     //Disabling refresh
@@ -22,9 +21,10 @@ export default class App extends Component {
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     const API_KEY = '1de36f8c19f786cefa15b4f8520c2604'
+    //const units = "metric"
 
     //Calling API with variables above
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
     //Parsing data to json
     const data = await api_call.json()
 
@@ -48,20 +48,31 @@ export default class App extends Component {
   }
   render() {
     return (
-      <div>
-        <Title />
-        <Form 
-        //Passing Method to Form.js
-        getWeather={this.getWeather} />
-        <Weather
-        //Passing fetched data to weather.js 
-        temperature={this.state.temperature}
-        city={this.state.city}
-        country={this.state.country}
-        humidity={this.state.humidity}
-        description={this.state.description}
-        error={this.state.error}
-        />
+
+      <div className="wrapper">
+        <div className="main">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-xs-5 title-container">
+                <Title />
+                <div className="col-xs-7 form-container">
+                  <Form 
+                  //Passing Method to Form.js
+                  getWeather={this.getWeather} />
+                  <Weather
+                  //Passing fetched data to weather.js 
+                  temperature={this.state.temperature}
+                  city={this.state.city}
+                  country={this.state.country}
+                  humidity={this.state.humidity}
+                  description={this.state.description}
+                  error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -3,15 +3,25 @@ import React, { Component } from 'react'
 export default class Weather extends Component {
   render() {
     return (
-      <div>
+      <div className="weather__info">
         {/*Hate this!!! Needs work*/}
         
-        { this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p> }
+        { 
+        this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p> 
+        }
 
-        { this.props.temperature && <p>Temperature: {this.props.temperature}</p> }
-        { this.props.humidity && <p>Humidity: {this.props.humidity}</p> }
-        { this.props.description && <p>Conditions: {this.props.description}</p> }
-        { this.props.error && <p>{this.props.error}</p>} 
+        {
+        this.props.temperature && <p>Temperature: {this.props.temperature}</p> 
+        }
+        {
+        this.props.humidity && <p>Humidity: {this.props.humidity}</p> 
+        }
+        {
+        this.props.description && <p>Conditions: {this.props.description}</p> 
+        }
+        {
+        this.props.error && <p>{this.props.error}</p>
+        } 
       </div>
     )
   }
